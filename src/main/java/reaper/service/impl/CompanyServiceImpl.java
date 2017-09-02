20d15af9e0df8c6f3fb081493276b03a5397aa66
151099149 <151099149@smail.nju.edu.cn>
2017-09-02 16:46:04 +0800

    modify CompanyServiceImpl

--- src/main/java/reaper/service/impl/CompanyServiceImpl.java
+++ src/main/java/reaper/service/impl/CompanyServiceImpl.java
@@ -1,5 +1,6 @@
 package reaper.service.impl;
 
+import org.springframework.stereotype.Service;
 import reaper.bean.FundPerformanceBean;
 import reaper.bean.ManagerPerformanceBean;
 import reaper.service.CompanyService;
@@ -9,7 +10,10 @@ import java.util.List;
 /**
  * Created by Feng on 2017/9/2.
  */
+
+@Service
 public class CompanyServiceImpl implements CompanyService {
+
     @Override
     public List<FundPerformanceBean> findFundPerformanceByCompanyId(String companyId) {
         return null;
