eb90b2b51c40dc1abb6614cea5ba8663dee1655f
151250162 <151250162@smail.nju.edu.cn>
2017-09-05 21:17:28 +0800

    manager test v1

--- src/test/java/reaper/service/ManagerServiceTest.java
+++ src/test/java/reaper/service/ManagerServiceTest.java
@@ -1,5 +1,6 @@
 package reaper.service;
 
+import org.hibernate.mapping.Collection;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +9,11 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import reaper.bean.FundHistoryBean;
 import reaper.bean.ManagerBean;
 
+import java.text.SimpleDateFormat;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
+import java.util.OptionalInt;
 
 import static org.junit.Assert.*;
 
@@ -19,35 +24,59 @@ import static org.junit.Assert.*;
 @SpringBootTest()
 public class ManagerServiceTest {
 
+    private SimpleDateFormat sdf=new SimpleDateFormat("yyyyMMddHHmmss");
+
     @Autowired
     ManagerService managerService;
 
-    //测试不存在的id
+    //测试根据managerId查找manager
+
+    //测试不存在的id=0
     @Test
     public void findManagerById1() throws Exception {
         ManagerBean managerBean=managerService.findManagerById("0");
         assertEquals(null,managerBean);
     }
 
-    //测试存在的id
+    //测试存在的id=30304626
     @Test
     public void findManagerById2() throws Exception{
         ManagerBean managerBean=managerService.findManagerById("30304626");
         assertEquals("金梓才",managerBean.name);
         assertEquals(67.91,managerBean.bestReturns,0.000001);
+        assertEquals("80161341",managerBean.company.id);
     }
 
-    //测试不存在的id
+    //测试根据managerId查找fundHistory
+
+    //测试不存在的id=0
     @Test
     public void findFundHistoryById1() throws Exception {
         List<FundHistoryBean> fundHistoryBeans=managerService.findFundHistoryById("0");
         assertEquals(0,fundHistoryBeans.size());
     }
 
+    //测试存在的id=30413691
     @Test
     public void findFundHistoryById2() throws Exception {
-        List<FundHistoryBean> fundHistoryBeans=managerService.findFundHistoryById("30036320");
-//        assertEquals(34,fundHistoryBeans.size());
+        List<FundHistoryBean> fundHistoryBeans=managerService.findFundHistoryById("30413691");
+        int code=fundHistoryBeans.stream().mapToInt((x)->Integer.parseInt(x.id)).summaryStatistics().getMax();
+        int time=fundHistoryBeans.stream().mapToInt((x)->x.days).summaryStatistics().getMin();
+        assertEquals(2,fundHistoryBeans.size());
+        assertEquals(60,time);
+        assertEquals(4476,code);
+    }
+
+    //测试存在的id=30045022
+    @Test
+    public void findFundHistoryById3() throws Exception {
+        List<FundHistoryBean> fundHistoryBeans=managerService.findFundHistoryById("30045022");
+        fundHistoryBeans.sort(Comparator.comparing(h -> h.id));
+        System.out.println(fundHistoryBeans.get(0).id);
+        assertEquals(21,fundHistoryBeans.size());
+        assertEquals(39.67,fundHistoryBeans.get(0).returns,0.000001);
+        assertEquals("中低风险",fundHistoryBeans.get(0).type.get(1));
+        assertEquals(796,fundHistoryBeans.get(16).days.intValue());
     }
 
     @Test
