18531da673c66fa24962bf913c75d28d58913644
151250022 <151250022@smail.nju.edu.cn>
2017-09-10 14:18:46 +0800

    managerremark

---
+++ src/main/java/reaper/repository/ManagerRemarkRepository.java
@@ -0,0 +1,10 @@
+package reaper.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import reaper.model.ManagerRemark;
+
+/**
+ * Created by max on 2017/9/10.
+ */
+public interface ManagerRemarkRepository extends JpaRepository<ManagerRemark,Integer> {
+}
