3099a97706afc9c5f52819c00fa5d5d401e13e1e
151250162 <151250162@smail.nju.edu.cn>
2017-09-12 17:10:15 +0800

    company test v1

--- src/test/java/reaper/service/CompanyServiceTest.java
+++ src/test/java/reaper/service/CompanyServiceTest.java
@@ -79,6 +79,7 @@ public class CompanyServiceTest {
         });
     }
 
+    //TODO rate有问题
     //存在的companyId=80036797，managers部分，结合白盒测试
     @Test
     public void findManagerPerformanceByCompanyId2() throws Exception{
@@ -130,7 +131,17 @@ public class CompanyServiceTest {
     @Test
     public void findStyleAttributionProfitByCompanyId2() throws Exception{
         List<FieldValueBean> fieldValueBeans=companyService.findStyleAttributionProfitByCompanyId("80041198");
-        assertEquals(10,companyService);
+        assertArrayEquals(new String[]{"10","beta"},new String[]{
+                fieldValueBeans.size()+"",
+                fieldValueBeans.get(0).field,
+        });
+        assertEquals(-0.0006704067896377638,fieldValueBeans.get(1).value,0.000001);
+    }
+
+    //factor_result中不存在的companyId=80560392,结合白盒测试
+    @Test
+    public void findStyleAttributionProfitByCompanyId3() throws Exception{
+        assertEquals(null,companyService.findStyleAttributionProfitByCompanyId("80560392"));
     }
 
     //根据公司id获得公司风格归因-主动风险
@@ -142,6 +153,23 @@ public class CompanyServiceTest {
         assertEquals(null,companyService.findStyleAttributionRiskByCompanyId("abc"));
     }
 
+    //存在的companyId=80000229，结合白盒测试
+    @Test
+    public void findStyleAttributionRiskByCompanyId2() throws Exception{
+        List<FieldValueBean> fieldValueBeans=companyService.findStyleAttributionRiskByCompanyId("80000229");
+        assertArrayEquals(new String[]{"10","价值"},new String[]{
+            fieldValueBeans.size()+"",
+            fieldValueBeans.get(1).field
+        });
+        assertEquals(-0.00005608566791481931,fieldValueBeans.get(2).value,0.000001);
+    }
+
+    //factor_result中不存在的companyId=80538609,结合白盒测试
+    @Test
+    public void findStyleAttributionRiskByCompanyId3() throws Exception{
+        assertEquals(null,companyService.findStyleAttributionRiskByCompanyId("80538609"));
+    }
+
     //根据公司id获得公司行业归因-主动收益
 
     //不存在companyId=0，abc
@@ -151,6 +179,23 @@ public class CompanyServiceTest {
         assertEquals(null,companyService.findIndustryAttributionProfitByCompanyId("abc"));
     }
 
+    //存在的companyId=80036782，结合白盒测试
+    @Test
+    public void findIndustryAttributionProfitByCompanyId2() throws Exception{
+        List<FieldValueBean> fieldValueBeans=companyService.findIndustryAttributionProfitByCompanyId("80036782");
+        assertArrayEquals(new String[]{"30","综合"},new String[]{
+                fieldValueBeans.size()+"",
+                fieldValueBeans.get(0).field
+        });
+        assertEquals(0.0023966964630367836,fieldValueBeans.get(1).value,0.000001);
+    }
+
+    //factor_result中不存在的companyId=80488954,结合白盒测试
+    @Test
+    public void findIndustryAttributionProfitByCompanyId3() throws Exception{
+        assertEquals(null,companyService.findIndustryAttributionProfitByCompanyId("80488954"));
+    }
+
     //根据公司id获得公司行业归因-主动风险
 
     //不存在companyId=0，abc
@@ -158,6 +203,23 @@ public class CompanyServiceTest {
     public void findIndustryAttributionRiskByCompanyId1() throws Exception {
         assertEquals(null,companyService.findIndustryAttributionRiskByCompanyId("0"));
         assertEquals(null,companyService.findIndustryAttributionRiskByCompanyId("abc"));
+    }
+
+    //存在的companyId=80365986
+    @Test
+    public void findIndustryAttributionRiskByCompanyId2() throws Exception{
+        List<FieldValueBean> fieldValueBeans=companyService.findIndustryAttributionRiskByCompanyId("80365986");
+        assertArrayEquals(new String[]{"30","机械"},new String[]{
+            fieldValueBeans.size()+"",
+            fieldValueBeans.get(1).field
+        });
+        assertEquals(-0.1577792721384206,fieldValueBeans.get(2).value,0.0000001);
+    }
+
+    //factor_result中不存在的companyId=80056613,结合白盒测试
+    @Test
+    public void findIndustryAttributionRiskByCompanyId3() throws Exception{
+        assertEquals(null,companyService.findIndustryAttributionRiskByCompanyId("80056613"));
     }
 
 }
