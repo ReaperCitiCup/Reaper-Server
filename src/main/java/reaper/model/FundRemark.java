3c347acadf6e8faf27c8488abcc1b40dd86acc01
151250022 <151250022@smail.nju.edu.cn>
2017-09-13 15:06:15 +0800

    hh

---
+++ src/main/java/reaper/model/FundRemark.java
@@ -0,0 +1,16 @@
+package reaper.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by max on 2017/9/13.
+ */
+@Entity
+@Table(name = "fund_remark")
+public class FundRemark {
+
+
+
+
+}
