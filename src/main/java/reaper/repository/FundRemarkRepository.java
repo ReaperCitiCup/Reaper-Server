5bc9618447204e15021aef2dea53be3a7afceed6
151250022 <151250022@smail.nju.edu.cn>
2017-09-14 14:59:18 +0800

    edge

--- src/main/java/reaper/repository/FundRemarkRepository.java
+++ src/main/java/reaper/repository/FundRemarkRepository.java
@@ -2,10 +2,11 @@ package reaper.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import reaper.model.FundRankByType;
+import reaper.model.FundRemark;
 
 import java.util.List;
 
-public interface FundRemarkRepository extends JpaRepository<FundRankByType,Integer>{
+public interface FundRemarkRepository extends JpaRepository<FundRemark,Integer>{
     public List<FundRankByType> findAllByCode(String code);
 
 
