96bc3bfc58cc431e614345b3f8e32e47cfcd4cd8
151250206 <151250206@smail.nju.edu.cn>
2017-10-31 20:03:21 +0800

    <feat> : <增加定时器>

---
+++ src/main/java/reaper/util/SchedulerUtil.java
@@ -0,0 +1,19 @@
+package reaper.util;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author keenan on 31/10/2017
+ */
+@Component
+public class SchedulerUtil {
+    //    @Scheduled(cron = "0 0 3 * * ?")
+    @Scheduled(fixedRate = 3000)
+    public void sayHello() throws Exception {
+        System.out.println("Hello " + LocalDateTime.now());
+    }
+
+}
